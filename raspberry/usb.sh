bd9537c9389da8a2b8ca54777d48e5d6ebfbe44d
Morten A. Middelthon <morten.middelthon@lyse.no>
2017-02-04 14:07:52 +0100

    Set up USB serial device on Raspberry Pi

---
+++ raspberry/usb.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+SPEED=9600
+DEVICE=/dev/ttyUSB0
+stty -F $DEVICE cs8 $SPEED ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -cooked -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
+cat < $DEVICE
