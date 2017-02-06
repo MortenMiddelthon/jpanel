e3e8f8c62c55e58a3697c975eab3a32f6e3e9a8c
Morten A. Middelthon <morten.middelthon@lyse.no>
2017-02-06 15:42:34 +0100

    Set TX/RX mode for transciever when sending

--- button_box/button_box.ino
+++ button_box/button_box.ino
@@ -1,11 +1,20 @@
-#define WHITE 5
-#define RED 6
+#include <SoftwareSerial.h>
+#define SSerialRX        4  //Serial Receive pin
+#define SSerialTX        3  //Serial Transmit pin
+#define SSerialTxControl 5
+SoftwareSerial RS485(SSerialRX, SSerialTX); // RX, TX
+#define RS485Transmit    HIGH
+#define RS485Receive     LOW
+
+#define WHITE 1
+#define RED 0
 #define ID0 12
 #define ID1 13
 #define BUTTON_WHITE 0
 #define BUTTON_RED 1
 
-uint8_t white = 0, red = 0, white_state = 0, red_state = 0;
+uint16_t white = 0, red = 0;
+uint8_t white_state = 0, red_state = 0;
 uint8_t id = 0;
 
 
@@ -15,6 +24,11 @@ void setup () {
 	pinMode(RED, INPUT);
 	pinMode(ID0, INPUT);
 	pinMode(ID1, INPUT);
+	pinMode(SSerialTxControl, OUTPUT);    
+	digitalWrite(SSerialTxControl, RS485Receive);
+	RS485.begin(9600);   // set the data rate 
+
+
 	id += digitalRead(ID0);
 	id += digitalRead(ID1);
 	Serial.println("[Button Box]");
@@ -24,22 +38,22 @@ void setup () {
 }
 
 void loop () {
-	white = digitalRead(WHITE);
-	red = digitalRead(RED);
-	if(white == LOW && white_state == 0) {
+	white = analogRead(WHITE);
+	red = analogRead(RED);
+	if(white < 500 && white_state == 0) {
 		white_state = 1;
 		txButton(BUTTON_WHITE);
 		delay(200);
 	}
-	else if(white == HIGH && white_state == 1) {
+	else if(white > 500 && white_state == 1) {
 		white_state = 0;
 	}
-	if(red == LOW && red_state == 0) {
+	if(red < 500 && red_state == 0) {
 		red_state = 1;
 		txButton(BUTTON_RED);
 		delay(200);
 	}
-	else if(red == HIGH && red_state == 1) {
+	else if(red > 500 && red_state == 1) {
 		red_state = 0;
 	}
 	delay(50);
@@ -49,4 +63,10 @@ void txButton(uint8_t button) {
 	Serial.print("#"); Serial.print(id); Serial.print(":");
 	Serial.print(button);
 	Serial.println(";");
+	digitalWrite(SSerialTxControl, RS485Transmit);
+	RS485.print("#"); RS485.print(id); RS485.print(":");
+	RS485.print(button);
+	RS485.println(";");
+	digitalWrite(SSerialTxControl, RS485Receive);
+	delay(50);
 }
