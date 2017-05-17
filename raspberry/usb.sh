5f9a6c1915f1f3920cbdf6ebaf51a3cb44c5e320
Morten A. Middelthon <morten@flipp.net>
2017-05-17 21:38:55 +0200

    misc client ui updates

--- raspberry/usb.sh
+++ raspberry/usb.sh
@@ -2,4 +2,4 @@
 SPEED=9600
 DEVICE=/dev/ttyUSB0
 stty -F $DEVICE cs8 $SPEED ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -cooked -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
-cat < $DEVICE
+# cat < $DEVICE
