ef50ca6343d12aa37d41beb941854688e7432d6d
Morten A. Middelthon <morten@flipp.net>
2018-03-02 17:10:02 +0100

    Oppdatering for RM 2018

--- raspberry/html/js/display_client.js
+++ raspberry/html/js/display_client.js
@@ -30,7 +30,7 @@ $(function () {
     connection.onopen = function () {
         // first we want users to enter their names
         input.removeAttr('disabled');
-        main_text.innerHTML = "Rogalandsmesterskap 2017";
+        main_text.innerHTML = "RM S&oslash;rVest 2018";
     };
 
     connection.onerror = function (error) {
